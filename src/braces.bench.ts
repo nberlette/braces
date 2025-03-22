1385d4ec1c503e4572e021fe15ad1b9de3a9020f
Nicholas Berlette <nick@berlette.com>
2025-03-21 18:18:22 -0700

    perf: remove slow wasm from braces bench

--- src/braces.bench.ts
+++ src/braces.bench.ts
@@ -1,16 +1,4 @@
-import * as js from "./braces.ts";
-import * as wasm from "../wasm/lib/braces.js";
-import * as wasm_sync from "../wasm/lib/braces_sync.js";
-import * as wasm_old from "../wasm/lib-old-2/braces.js";
-import * as wasm_old_sync from "../wasm/lib-old-2/braces.js";
-
-const targets = [
-  ["js", js],
-  ["wasm", wasm],
-  ["wasm_old", wasm_old],
-  ["wasm_sync", wasm_sync],
-  ["wasm_old_sync", wasm_old_sync],
-] as const;
+import { braces } from "./braces.ts";
 
 const tests = [
   { input: "server/{node,deno}/{v1,v2,v3}/app.{js,ts}" },
@@ -20,16 +8,13 @@ const tests = [
 
 const iterations = [100, 250, 500, 1000] as const;
 
-for (const [name, { braces }] of targets) {
-  for (const { input } of tests) {
-    for (const n of iterations) {
-      Deno.bench({
-        group: `[n=${n}] [input=${JSON.stringify(input)}]`,
-        name: `[${name}]`,
-        n,
-        baseline: name === "js",
-        fn: () => void braces(input),
-      });
-    }
+for (const { input } of tests) {
+  for (const n of iterations) {
+    Deno.bench({
+      group: `[n=${n}]`,
+      name: `[braces] [input=${JSON.stringify(input)}]`,
+      n,
+      fn: () => void braces(input),
+    });
   }
 }
